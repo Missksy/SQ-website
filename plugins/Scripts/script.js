f5e78e8727531b8989f916e0c2e5852267539cfd
cecilia.santos <cecilia.santos@zenn.pt>
2019-06-15 20:03:13 +0100

    search tablet with pagination and button for details and another button to push to top

--- plugins/Scripts/script.js
+++ plugins/Scripts/script.js
@@ -1,5 +1,11 @@
 $(function () {
 
+    //push to top
+    $('.btn-pushToTop').click(function (e) {
+        e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+    });
+
     var urlcontacts = "http://contactsqs2.apphb.com/Service.svc/rest/contacts";
 
     // create dinamic table
@@ -14,6 +20,7 @@ $(function () {
             html += '<td>' + items[i].Email + '</td>';
             html += '<td>' + items[i].Phone + '</td>';
             html += '<td>' + items[i].Source + '</td>';
+            html += '<td class="text-center"><a href="details.html?guid=' + items[i].Guid + '" class="btn btn-outline-primary size--s12">ver mais</a></td>';
             html += '</tr>';
         }
         $(table).append(html);
@@ -27,7 +34,23 @@ $(function () {
             data: {},
             url: urlcontacts,
             success: function (data) {
-                table(data);
+                $('#Contacts').DataTable({
+                    data: data,
+                    "columns": [
+                        { "data": "GivenName" },
+                        { "data": "Surname" },
+                        { "data": "Email" },
+                        { "data": "Phone" },
+                        { "data": "Source" },
+                        {
+                            "data": "Guid",
+                            orderable: false,
+                            render: function (data, type, row) {
+                                return '<div class="text-center"><a href="details.html?guid=' + data + '" class="btn btn-outline-primary size--s12">ver mais</a></div>';
+                            }
+                        }
+                    ]
+                });
             }
         });
     }
