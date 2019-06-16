c805d33b54bb64f7e01caa1b7a03cd0fd130cf07
Mayelson Sousa <msousa@humansoft.pt>
2019-06-16 23:01:16 +0100

    filter by source

--- scripts/index.js
+++ scripts/index.js
@@ -29,6 +29,26 @@ $(function () {
     //    $(table).append(html);
     //}
 
+    function table(data) {
+        $('#Contacts').DataTable({
+            data: data,
+            "columns": [
+                { "data": "GivenName" },
+                { "data": "Surname" },
+                { "data": "Email" },
+                { "data": "Phone" },
+                { "data": "Source" },
+                {
+                    "data": "Guid",
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '<div class="text-center"><a href="details.html?guid=' + data + '" class="btn btn-outline-primary size--s12">ver mais</a></div>';
+                    }
+                }
+            ]
+        });
+    }
+
     // Get all contacts
     function bindContacts() {
         $.ajax({
@@ -37,26 +57,33 @@ $(function () {
             data: {},
             url: urlcontacts,
             success: function (data) {
-                $('#Contacts').DataTable({
-                    data: data,
-                    "columns": [
-                        { "data": "GivenName" },
-                        { "data": "Surname" },
-                        { "data": "Email" },
-                        { "data": "Phone" },
-                        { "data": "Source" },
-                        {
-                            "data": "Guid",
-                            orderable: false,
-                            render: function (data, type, row) {
-                                return '<div class="text-center"><a href="details.html?guid=' + data + '" class="btn btn-outline-primary size--s12">ver mais</a></div>';
-                            }
-                        }
-                    ]
-                });
+                table(data);
             }
         });
     }
+
+     //get contacts by Source
+    var urlSource = "http://contactsqs2.apphb.com/Service.svc/rest/contacts/bysource/";
+
+    $("#FilterBySource").change(function (e) {
+        e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+        var SourceName = $(this).find(':selected').val();
+        if (SourceName == "0") {
+            bindContacts();
+        } else {
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                data: { source: SourceName },
+                url: urlSource + SourceName,
+                success: function (data) {
+                    $('#Contacts').DataTable().clear().destroy();
+                    table(data);
+                }
+            });
+        }
+    });
+
 
     bindContacts();
 });
